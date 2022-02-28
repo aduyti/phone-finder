05db387b18f7b5f884ceda14288ce579134e2985
Anirudhya <anirudhyaduyti@gmail.com>
2022-03-01 00:15:47 +0600

    load search data

--- script/app.js
+++ script/app.js
@@ -3,10 +3,28 @@ document.getElementById('search-button').addEventListener('click', () => {
     const input = document.getElementById('search-input');
     if (input.value) {
         searchInput = input.value;
-        console.log(searchInput.toLowerCase());
+        loadSearchData(searchInput.toLowerCase());
     }
     else {
+        // error message 
         console.log('Type something to search');
     }
     input.value = '';
 });
+
+const loadSearchData = async (searchKey) => {
+    // const phoneDetailsURL = "https://openapi.programming-hero.com/api/phone/";
+    const dataURL = `https://openapi.programming-hero.com/api/phones?search=${searchKey}`;
+
+    const response = await fetch(dataURL);
+    const data = await response.json();
+    showSearchResults(data.data);
+};
+const showSearchResults = phones => {
+    if (phones.length) {
+        phones.forEach(phone => console.log(phone));
+    }
+    else {
+        console.log('No phone found');
+    }
+};
