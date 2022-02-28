5156f37f7e1ec8618d8946f9b965f4e1c7ac23a4
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-28 22:35:53 +0600

    get input on click

--- script/app.js
+++ script/app.js
@@ -1,1 +1,12 @@
-console.log(4);
+let searchInput = '';
+document.getElementById('search-button').addEventListener('click', () => {
+    const input = document.getElementById('search-input');
+    if (input.value) {
+        searchInput = input.value;
+        console.log(searchInput.toLowerCase());
+    }
+    else {
+        console.log('Type something to search');
+    }
+    input.value = '';
+});
